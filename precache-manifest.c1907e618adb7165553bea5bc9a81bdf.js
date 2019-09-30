f7a94766a6dd2e068c1a261173a9175576faf08c
Ivan Sedakov <lazofl24@gmail.com>
2019-09-30 18:49:17 +0200

    Updates

---
+++ precache-manifest.c1907e618adb7165553bea5bc9a81bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0352fc91b80a4af28090b0ee514841b0",
+    "url": "/testpanda/index.html"
+  },
+  {
+    "revision": "9df5048f01dbbfacf229",
+    "url": "/testpanda/static/css/main.1200e5d7.chunk.css"
+  },
+  {
+    "revision": "a2d40095e28a948a4893",
+    "url": "/testpanda/static/js/2.6b9adc64.chunk.js"
+  },
+  {
+    "revision": "9df5048f01dbbfacf229",
+    "url": "/testpanda/static/js/main.41bc5242.chunk.js"
+  },
+  {
+    "revision": "7963eeda41d297653031",
+    "url": "/testpanda/static/js/runtime~main.cd319715.js"
+  },
+  {
+    "revision": "e93567914c3639f8137567ffd318231e",
+    "url": "/testpanda/static/media/errorAuth.e9356791.png"
+  },
+  {
+    "revision": "6f6e8929f7a562260a752d165685564d",
+    "url": "/testpanda/static/media/mathPanda.6f6e8929.png"
+  }
+]);
